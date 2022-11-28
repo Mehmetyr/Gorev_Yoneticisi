2e962a4fc5206e8262a3cf628965cdb70003d784
Mehmetyr <96690651+Mehmetyr@users.noreply.github.com>
2022-11-28 13:42:37 +0300

    Gorev_Yoneticisi
    .Net Core ile Swagger ve Jwt ile geliştirilen kullanıcının günlük, haftalık ve aylık olarak planlama yaptığı uygulama

---
+++ Plans/Plans.API/Models/UserConstants.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Plans.API.Models
+{
+    public class UserConstants
+    {
+        public static List<UserModel> users = new List<UserModel>()
+        {
+            new UserModel(){ UserName = "Mehmet_Admin", EmailAddress="mhmtyru@gmail.com",Password="1234",GivenName="Mehmet",Surname="Yoru",Role="Admin"},
+            new UserModel(){ UserName = "Yusuf_Kullanici", EmailAddress="Yusuf@gmail.com",Password="1234",GivenName="Yusuf",Surname="Yoru",Role="Kullanici"}
+
+        };
+    }
+}
---
+++ Plans/Plans.API/Startup.cs
@@ -0,0 +1,97 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Tokens;
+using Plans.Business.Abstract;
+using Plans.Business.Concrete;
+using Plans.DataAcessLayer.Abstract;
+using Plans.DataAcessLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Plans.API
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+            var Key = Configuration.GetValue<string>("Key");
+            var ýssuer = Configuration.GetValue<string>("Issuer");
+            var audience = Configuration.GetValue<string>("Audience");
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options =>
+                {
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuer = true,
+                        ValidateAudience = true,
+                        ValidateLifetime = true,
+                        ValidateIssuerSigningKey = true,
+                        ValidIssuer = Configuration["Jwt:Issuer"],
+                        ValidAudience = Configuration["Jwt:Audience"],
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["Jwt:Key"]))
+                    };
+                });
+
+
+            services.AddMvc();
+            services.AddControllers();
+            services.AddSingleton<IPlansService, PlansManager>();
+            services.AddSingleton<IPlansRepository, PlansRepository>();
+            services.AddSwaggerDocument(configure=>
+            {
+                configure.PostProcess = (doc =>
+                {
+                    doc.Info.Title = "Planlar Api";
+                    doc.Info.Version = "1.0.26";
+                    doc.Info.Contact = new NSwag.OpenApiContact()
+                    {
+                        Name = "Mehmet Yörü",                       
+                        Email = "mhmtyru@gmail.com"
+                    };
+                });
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+            }
+
+            app.UseStaticFiles();
+
+            app.UseRouting();
+            app.UseOpenApi();
+            app.UseSwaggerUi3();
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
